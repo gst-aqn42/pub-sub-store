5c30375df450553ab98efc4b2298bb020cbef1d7
gst-aqn42 <gus.aquino42.com>
2025-10-29 23:02:53 -0300

    Tarefa prática #2 - PubSub (versão final com relatório)

--- services/report/app.js
+++ services/report/app.js
@@ -23,8 +23,40 @@ async function printReport() {
       }
 }
 
+
+async function processMessage(msg) {
+    try {
+        const reportData = JSON.parse(msg.content.toString());
+        console.log('--- [REPORT SERVICE] NEW MESSAGE RECEIVED ---');
+        
+        await updateReport(reportData.products);
+        await printReport();
+        
+        console.log('-------------------------------------------');
+    } catch (error) {
+        console.log(`X ERROR TO PROCESS: ${error.message}`);
+        console.log(msg.content.toString());
+    }
+}
+
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    console.log('Starting Report Service...');
+    let rabbitMQService;
+    try {
+        rabbitMQService = await RabbitMQService.getInstance();
+    } catch (error) {
+        console.error('X ERROR: Could not connect to RabbitMQ.', error);
+        process.exit(1);
+    }
+
+    const queue = 'report';
+    console.log(`[REPORT SERVICE] Waiting for messages in queue: ${queue}`);
+
+    try {
+        await rabbitMQService.consume(queue, processMessage);
+    } catch (error) {
+        console.error(`X ERROR: Could not start consuming from queue ${queue}`, error);
+    }
 } 
 
 consume()
